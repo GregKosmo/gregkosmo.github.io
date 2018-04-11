06f99477fcef9833a53fbb7b83894d8a3fdcc743
Greg <greg@gregkosmacek.com>
2018-04-10 22:35:53 -0700

    remove logging from js, shrink css

--- sw.js
+++ sw.js
@@ -11,14 +11,12 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
 self.addEventListener('fetch', function(event){
-    console.log(event.request.url);
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
