dd14761399e78a02fbb573db1919598700258fe5
Greg <greg@gregkosmacek.com>
2018-04-15 23:49:38 -0700

    menu mobile responsiveness

--- script.js
+++ script.js
@@ -1,10 +1,65 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-      }, function(err) {
-        // registration failed :(
+var navBar = document.getElementById("navBar");
+var menuItems = navBar.getElementsByClassName("menuItem");
+var firstToggle = true;
+var toggled = false;
+
+init = function() {
+  if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+
+        }, function(err) {
+          console.log(err);
       });
     });
   }
-  
+
+  if(window.innerWidth > 768) {
+    for(var i = 0; i < menuItems.length; i++) {
+      var menuItem = menuItems[i];
+      menuItem.removeAttribute("style");
+    }
+    
+  } else {
+    for(var i = 0; i < menuItems.length; i++) {
+      var menuItem = menuItems[i];
+      menuItem.setAttribute("style", "display: none");
+    }
+  }
+}
+
+init();
+
+var toggleMenu = function() {
+  for(var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    if(menuItem.hasAttribute("style")) {
+      menuItem.removeAttribute("style");
+      toggled = true;
+    } else {
+      menuItem.setAttribute("style", "display: none");
+      toggled = false;
+    }
+  }
+}
+
+window.onresize = function() {
+  if(window.innerWidth > 768) {
+    for(var i = 0; i < menuItems.length; i++) {
+      var menuItem = menuItems[i];
+      menuItem.removeAttribute("style");
+    }
+
+    firstToggle = true;
+
+  } else {
+    if(firstToggle && !toggled) {
+      for(var i = 0; i < menuItems.length; i++) {
+        var menuItem = menuItems[i];
+        menuItem.setAttribute("style", "display: none");
+      }
+
+      firstToggle = false;
+    }
+  }
+}
