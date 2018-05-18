c31ea2ae7dcc672dd82d6985dd5c4e86e0e0668b
Greg <greg@gregkosmacek.com>
2018-05-18 00:14:46 -0700

    Add fontAwesome, probably break website, dynamic loading js&css

--- main.js
+++ main.js
@@ -123,3 +123,15 @@ window.onresize = function() {
     }
   }
 }
+toUpperCase = function(fromId, toId) {
+    document.getElementById(toId).innerText = document.getElementById(fromId).value.toUpperCase();
+}
+
+toLowerCase = function(fromId, toId) {
+    document.getElementById(toId).innerText = document.getElementById(fromId).value.toLowerCase();
+}
+
+characterCount = function(fromId, toId) {
+    var length = document.getElementById(fromId).value.length;
+    document.getElementById(toId).innerText = length > 0 ? length : '';
+}
