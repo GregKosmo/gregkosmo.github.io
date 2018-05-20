43ee07dfc6707d535b267e95623e849b15b29832
Greg <greg@gregkosmacek.com>
2018-05-20 11:56:21 -0700

    Fix service worker js and css caching

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  '/dist/**.*.css',
-  '/dist/**.*.js'
+  '/dist/*.css',
+  '/dist/*.js'
 ];
 
 self.addEventListener('install', function(event) {
