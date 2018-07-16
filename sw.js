0cddc46e79142220f985fce8fe0dd8e7ad0428b6
Greg <greg@gregkosmacek.com>
2018-07-15 21:06:27 -0500

    Manifest, service worker, icons

---
+++ sw.js
@@ -0,0 +1,23 @@
+var CACHE_NAME = 'gregkosmo-site-cache';
+var urlsToCache = [
+  '/*.css',
+  '/*.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
